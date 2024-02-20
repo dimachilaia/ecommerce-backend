7fee960bc95f01a3829f2a7e8f4bc1867ecd605b
Dimitri Tchilaia <dimitri@agronnect.com>
2024-02-20 18:19:20 +0400

    moved data to back folder

--- src/server.js
+++ src/server.js
@@ -1,10 +1,25 @@
 import express from "express";
+import { products, cartItems } from "./temp-data.js";
 
 const app = express();
 
 app.get("/hello", (req, res) => {
   res.send("Hello");
 });
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/cart", (req, res) => {
+  res.json(cartItems);
+});
+app.get("/products/:productId", (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find((product) => product.id === productId);
+  res.json(product);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
---
+++ src/temp-data.js
@@ -0,0 +1,83 @@
+// import firstImage from "./assets/bag.avif";
+// import secondImage from "./assets/bag.avif";
+// import image2 from "./assets/bag2.avif";
+export const products = [
+  {
+    id: "1",
+    name: "Bag",
+    // imageName: firstImage,
+    price: "$199",
+  },
+  {
+    id: "2",
+    name: "Bag",
+    // imageName: secondImage,
+    price: "$109",
+  },
+  {
+    id: "3",
+    name: "Bag",
+    // imageName: image2,
+    price: "$99",
+  },
+  {
+    id: "4",
+    name: "Bag",
+    // imageName: image2,
+    price: "$99",
+  },
+  {
+    id: "5",
+    name: "Bag",
+    // imageName: image2,
+    price: "$99",
+  },
+  {
+    id: "6",
+    name: "Bag",
+    // imageName: image2,
+    price: "$99",
+  },
+  {
+    id: "7",
+    name: "Bag",
+    // imageName: image2,
+    price: "$99",
+  },
+  {
+    id: "8",
+    name: "Bag",
+    // imageName: image2,
+    price: "$99",
+  },
+  {
+    id: "9",
+    name: "Bag",
+    // imageName: image2,
+    price: "$99",
+  },
+];
+
+export const cartItems = [
+  {
+    id: "1",
+    name: "Bag",
+    // imageName: firstImage,
+    price: "$199",
+    quantity: 1,
+  },
+  {
+    id: "2",
+    name: "Bag",
+    // imageName: secondImage,
+    price: "$109",
+    quantity: 1,
+  },
+  {
+    id: "3",
+    name: "Bag",
+    // imageName: image2,
+    price: "$99",
+    quantity: 1,
+  },
+];
